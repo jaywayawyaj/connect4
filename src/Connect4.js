54bc824dc0975c94a7d1e2e4fc21a996118b08cc
Jethro Whaley <jethro.whaley@gmail.com>
2019-04-26 11:50:12 +0100

    Pairing on Connect 4

--- src/Connect4.js
+++ src/Connect4.js
@@ -9,11 +9,33 @@ class ConnectFour {
             [' ', ' ', ' ', ' ', ' ', ' '],
             [' ', ' ', ' ', ' ', ' ', ' ']
         ];
+        this.tokens = {
+          1: 'x',
+          2: 'o'
+        }
     }
 
     selectColumn(columnNumber, player) {
-        // implement
+        this.board[5][columnNumber - 1] = this.tokens[player]
+        this.hasWon(player)
+        return this.board
     }
+
+    hasWon(player) {
+      let counter = 1
+      for(let i = 0; i < 5; i++) {
+        if(this.board[5][i] === this.tokens[player] && this.board[5][i] === this.board[5][i + 1]) {
+          counter += 1;
+        } else {
+          counter = 1;
+        }
+
+        if(counter === 4) {
+          this.inProgress = false
+          return `Player ${player} has won!`
+        }
+      }
+  }
 }
 
 module.exports = ConnectFour;
--- test/Connect4.test.js
+++ test/Connect4.test.js
@@ -13,4 +13,112 @@ describe('Connect4', () => {
             [' ', ' ', ' ', ' ', ' ', ' ']
         ]);
     });
+
+    test('player 1 can select column 1', () => {
+      const connect4 = new Connect4();
+
+      expect(connect4.selectColumn(1, 1)).toEqual([
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          ['x', ' ', ' ', ' ', ' ', ' ']
+      ]);
+    });
+
+    test('player 2 can select column 2, 2', () => {
+      const connect4 = new Connect4();
+
+      expect(connect4.selectColumn(2, 2)).toEqual([
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', 'o', ' ', ' ', ' ', ' ']
+      ]);
+    });
+
+    test('player 1 can win with 4 in a row horozintally', () => {
+      const connect4 = new Connect4();
+
+      expect(connect4.inProgress).toEqual(true)
+
+      connect4.selectColumn(1, 1)
+      connect4.selectColumn(2, 1)
+      connect4.selectColumn(3, 1)
+      connect4.selectColumn(4, 1)
+
+      expect(connect4.board).toEqual([
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          ['x', 'x', 'x', 'x', ' ', ' ']
+      ]);
+
+      expect(connect4.inProgress).toEqual(false);
+
+      expect(connect4.hasWon(1)).toEqual('Player 1 has won!')
+    });
+
+    test('player 1 can win with 4 in a row horozintally with gaps', () => {
+      const connect4 = new Connect4();
+
+      connect4.selectColumn(2, 1)
+      connect4.selectColumn(3, 1)
+      connect4.selectColumn(4, 1)
+      connect4.selectColumn(5, 1)
+
+      expect(connect4.board).toEqual([
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', 'x', 'x', 'x', 'x', ' ']
+      ]);
+
+      expect(connect4.inProgress).toEqual(false);
+    });
+
+    test('player 2 can win with 4 in a row horozintally with gaps', () => {
+      const connect4 = new Connect4();
+
+      connect4.selectColumn(2, 2)
+      connect4.selectColumn(3, 2)
+      connect4.selectColumn(4, 2)
+      connect4.selectColumn(5, 2)
+
+      expect(connect4.board).toEqual([
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', 'o', 'o', 'o', 'o', ' ']
+      ]);
+
+      expect(connect4.inProgress).toEqual(false);
+
+      expect(connect4.hasWon(2)).toEqual('Player 2 has won!')
+
+    });
+
+    test('player 1 can select column 1 twice', () => {
+      const connect4 = new Connect4();
+      connect4.selectColumn(1, 1);
+      connect4.selectColumn(1, 1);
+
+      expect(connect4.board).toEqual([
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' '],
+          ['x', ' ', ' ', ' ', ' ', ' '],
+          ['x', ' ', ' ', ' ', ' ', ' ']
+      ]);
+    });
 });
